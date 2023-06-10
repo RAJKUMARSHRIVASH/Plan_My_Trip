d4283c03302d9862b3058f3963da3eaeea4578ec
RAJ KUMAR SEN <rajkumarshrivash374@gmail.com>
2023-06-10 11:29:55 +0530

    deplopyment

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+
+const app = express();
+
+app.use(express.static(path.join(__dirname,"client","build")))
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+})
+
+app.use(express.json());
+
+app.listen(8000,()=>{
+    console.log("server is running at port 8000");
+
+})
