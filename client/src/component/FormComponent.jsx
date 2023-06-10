f74720fcc6a40edfba259d32f889ec1a387fe2bb
RAJ KUMAR SEN <rajkumarshrivash374@gmail.com>
2023-06-10 14:56:45 +0530

    post form created from chakra

--- client/src/component/FormComponent.jsx
+++ client/src/component/FormComponent.jsx
@@ -1,9 +1,115 @@
-import React from 'react'
+// import React from 'react'
+// import { Form } from 'react-router-dom'
 
-function FormComponent() {
+// function FormComponent() {
+//   return (
+//     <div>
+//       <form >
+
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default FormComponent
+
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  HStack,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+} from '@chakra-ui/react';
+import { Select } from '@chakra-ui/react'
+import { useState } from 'react';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+
+export default function FormComponent() {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div>FormComponent</div>
-  )
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'} textAlign={'center'}>
+            Post Your Trip
+          </Heading>
+          <Text fontSize={'lg'} color={'gray.600'}>
+            to enjoy all of our cool features ✌️
+          </Text>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <HStack>
+              <Box>
+                <FormControl id="firstName" isRequired>
+                  <FormLabel>Name</FormLabel>
+                  <Input type="text" />
+                </FormControl>
+              </Box>
+              <Box>
+                <FormControl id="destination">
+                  <FormLabel>Destination</FormLabel>
+                  <Select>
+                    <option value='India'>India</option>
+                    <option value='Africa'>Africa</option>
+                    <option value='Europe'>Europe</option>
+                    <option value='America'>America</option>
+                  </Select>
+                </FormControl>
+              </Box>
+            </HStack>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input type={showPassword ? 'text' : 'password'} />
+                <InputRightElement h={'full'}>
+                  <Button
+                    variant={'ghost'}
+                    onClick={() =>
+                      setShowPassword((showPassword) => !showPassword)
+                    }>
+                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Stack spacing={10} pt={2}>
+              <Button
+                loadingText="Submitting"
+                size="lg"
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}>
+                Sign up
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
 }
-
-export default FormComponent
