350fc8608b0ae9d9e0074d076faf97af1653a0f2
RAJ KUMAR SEN <rajkumarshrivash374@gmail.com>
2023-06-10 16:02:12 +0530

    deployment

--- client/src/component/FormComponent.jsx
+++ client/src/component/FormComponent.jsx
@@ -1,17 +1,3 @@
-// import React from 'react'
-// import { Form } from 'react-router-dom'
-
-// function FormComponent() {
-//   return (
-//     <div>
-//       <form >
-
-//       </form>
-//     </div>
-//   )
-// }
-
-// export default FormComponent
 
 import {
   Flex,
@@ -19,22 +5,58 @@ import {
   FormControl,
   FormLabel,
   Input,
-  InputGroup,
   HStack,
-  InputRightElement,
   Stack,
   Button,
   Heading,
   Text,
   useColorModeValue,
-  Link,
 } from '@chakra-ui/react';
 import { Select } from '@chakra-ui/react'
-import { useState } from 'react';
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+// import { useEffect } from 'react';
 
 export default function FormComponent() {
-  const [showPassword, setShowPassword] = useState(false);
+
+  const baseURL = "https://planmytrip-q0gy.onrender.com"
+  // const baseURL = "http://localhost:8000"
+
+
+  // const [name,setName] = useState("");
+  // const [destination,setDestination] = useState("");
+  // const [email,setEmail] = useState("");
+  // const [no_of_travellers,setNo] = useState("");
+  // const [budget_per_person,setBudget] = useState("");
+
+  async function postData() {
+    const name = document.getElementById("firstName").value;
+    const destination = document.getElementById("destination").value;
+    const email = document.getElementById("email").value;
+    const no_of_travellers = document.getElementById("no_of_traveler").value;
+    const budget_per_person = document.getElementById("budget_per_person").value;
+
+    const payload = {
+      name,
+      destination,
+      email,
+      no_of_travellers,
+      budget_per_person
+    }
+    // console.log(payload)
+
+    const fetctedData = await fetch(`${baseURL}/maketrip`, {
+      method: "POST",
+      header: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    });
+    const data = await fetctedData.json();
+    console.log(data)
+    if(data){
+      alert(data)
+    }
+
+  }
 
   return (
     <Flex
@@ -59,7 +81,7 @@ export default function FormComponent() {
           <Stack spacing={4}>
             <HStack>
               <Box>
-                <FormControl id="firstName" isRequired>
+                <FormControl id="firstName" >
                   <FormLabel>Name</FormLabel>
                   <Input type="text" />
                 </FormControl>
@@ -77,20 +99,21 @@ export default function FormComponent() {
                 </FormControl>
               </Box>
             </HStack>
-            <FormControl id="email" isRequired>
+            <FormControl id="email" >
               <FormLabel>Email address</FormLabel>
               <Input type="email" />
             </FormControl>
-            <FormControl id="no_of_traveler" isRequired>
+            <FormControl id="no_of_traveler" >
               <FormLabel>No. of Travellers</FormLabel>
               <Input type='Number'></Input>
             </FormControl>
-            <FormControl id="budget_per_person" isRequired>
+            <FormControl id="budget_per_person" >
               <FormLabel>Budget/Person</FormLabel>
               <Input type='Number'></Input>
             </FormControl>
             <Stack spacing={10} pt={2}>
               <Button
+                onClick={postData}
                 loadingText="Submitting"
                 size="lg"
                 bg={'blue.400'}
